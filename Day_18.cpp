67f5df5c30077eca3e93d5f27eac7568ff0f9362
Arjun19 <130502983+Ast1903@users.noreply.github.com>
2025-03-20 17:46:46 +0530

    Update Day_18.cpp
    DAY_18 complete

--- Day_18.cpp
+++ Day_18.cpp
@@ -6,3 +6,27 @@ class Solution {
             sort(t.begin(), t.end());
             return s == t;
         }};
+
+time complexity: O(n log n).
+space complexity: O(1).
+
+// problem 2(longest common perfix)
+class Solution {
+public:
+   string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+
+    string prefix = strs[0];  // Start with the first string
+
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) {  // Reduce prefix if it doesn't match
+            prefix = prefix.substr(0, prefix.length() - 1);
+            if (prefix.empty()) return "";
+        }
+    }
+    return prefix;
+}
+};
+
+time complexity: O(N×M)).
+space complexity: O(1).
